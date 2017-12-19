340f82c94b4d14ebbaba7488e4e33e219a295b32
Rodion Grishaev <radius486@gmail.com>
2017-12-19 20:57:59 +0300

    Vue project.

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <BitProfit/>
+  </div>
+</template>
+
+<script>
+import BitProfit from './components/bit-profit'
+
+export default {
+  name: 'app',
+  components: {
+    BitProfit
+  }
+}
+</script>
+
+<style lang="sass">
+#app
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
+
+</style>
---
+++ src/components/bit-profit.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrapper">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BitProfit',
+  data () {
+    return {
+      msg: 'Calculate profit.'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+h1,
+h2
+  font-weight: normal
+  color: #f0f
+
+.wrapper
+  max-width: 1200px
+  margin: 0 auto
+  padding: 0 20px
+
+</style>
